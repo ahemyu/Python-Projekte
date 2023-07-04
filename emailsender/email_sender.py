b9f78aa15478833bf19ca63d62f0cde1eeb83d7f
ahemyu <74073251+ahemyu@users.noreply.github.com>
2023-07-04 12:58:06 +0200

    Update email_sender.py

--- emailsender/email_sender.py
+++ emailsender/email_sender.py
@@ -4,16 +4,14 @@ from string import Template
 from pathlib import Path
 
 
-#oioxwtlwlyalciig
-
 html = Template(Path('index.html').read_text())
 
 email = EmailMessage()  # email objekt anlegen
 email['from'] = 'Hideaki Miyazaki'
 email['to'] = 'emovic@hotmail.de'
-email['subject'] = "You have biggest dicku"
+email['subject'] = "some text"
 
-email.set_content(html.substitute({'name': 'Schwanz'}), 'html')
+email.set_content(html.substitute({'name': 'SomeName'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:  #
     smtp.ehlo()  # Hello at beginning
